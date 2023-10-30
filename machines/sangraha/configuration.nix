daa1b93c1ea45c16672bbed8d6f19dfebdbaf90c
tchikmagalore <crthilakraj@gmail.com>
2023-10-30 20:51:50 +0100

    added ssh configuration

--- machines/sangraha/configuration.nix
+++ machines/sangraha/configuration.nix
@@ -2,8 +2,9 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, vscode-server, ... }:
-
+{ config, pkgs, vscode-server, ... }: let
+  username = "tchikmagalore";
+in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -53,12 +54,15 @@
   console.keyMap = "de";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.tchikmagalore = {
+  users.users.${username} = {
     isNormalUser = true;
-    description = "tchikmagalore";
+    description = "${username}";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       git
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDXMaiggm/K/Sa15wps/K2h5b/J/NDUYBotKhVRdoN5sI6WNcQWO29d/3/vLJgLN2ca4nS9np96E0G1+ZFG+hO+gL9BIviL37J6LsQLwEGiTrbnpZ8eATA6drv1IOeVSTJFuY8CzjerbrTD8u5v7kU8/Ib81x3wBLW5TC6KRL7sUWWNfgqP2WNfou6QraVDojTlWiqgNIuQiqIOGw6KX8plB5fRlGe4e23WoHBfC6lV+eBR9KEjzhVppegDkXP1tpDHr5Tca0x6+I8QAu7WEMNl5nIB9o4IMwXTxrikRv3WN0HzCLCzjlYBNUDlOxAwADs8qErHBeRLPtP/Btd+w7Az8SQVCswtDgA0FHlXGiWBiDznBNhXtrsj98QY17yII9LQgTpdSO4u15X0nGS1uOtxJZCukWcX7/pXsybzqIfH/tbC6Deh0Nd3ogyzZaNT5v89u6VNfvVTaVOsF7yE796qqUOlVhovIF0DrFhq7UW9703qTJa57lGyy4ekD1hB06k= tchikmagalore@tchikmagalore"
     ];
   };
 
@@ -83,7 +87,11 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
